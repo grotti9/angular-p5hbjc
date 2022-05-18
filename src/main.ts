18531def001a7817d74b3f4b837decb6593b7058
grotti9 <grotti9@hotmail.it>
2022-05-18 14:34:22 +0800

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,9 @@ import {NgbdTableCompleteModule} from './app/table-complete.module';
 platformBrowserDynamic()
     .bootstrapModule(NgbdTableCompleteModule)
     .then(ref => {
-      // Ensure Angular destroys itself on hot reloads.
       if (window['ngRef']) {
         window['ngRef'].destroy();
       }
       window['ngRef'] = ref;
-
-      // Otherwise, log the boot error
     })
     .catch(err => console.error(err));
